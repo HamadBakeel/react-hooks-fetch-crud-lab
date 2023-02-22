f56379348ddbf8b371160aaae330d49b9ba30069
unknown <hamad200714@gmail.com>
2023-02-22 22:44:29 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+    const [page, setPage] = useState("List");
+    const [questions, setQuestions] = useState([])
 
-  return (
-    <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
-    </main>
-  );
+    useEffect(()=>{
+        fetch("http://localhost:4000/questions").
+        then(r => r.json()).
+        then(data => setQuestions(data))
+    })
+
+    function handleDeleteItem(id){
+        const temp = questions.filter(question => question.id !== id)
+        setQuestions(temp)
+    }
+    function handleUpdateItem(data){
+        const temp = questions.map(question => {
+            if(question.id === data.id){
+                return data;
+            }
+            return question
+        })
+        setQuestions(temp)
+    }
+    return (
+        <main>
+            <AdminNavBar onChangePage={setPage}/>
+            {page === "Form"
+                ? <QuestionForm/>
+                : <QuestionList
+                    questions={questions}
+                    onDeleteQuestion={handleDeleteItem}
+                    onUpdateQuestion={handleUpdateItem}
+                />}
+        </main>
+    );
 }
 
 export default App;
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,63 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({question, onDeleteQuestion, onUpdateQuestion}) {
+    const {id, prompt, answers, correctIndex} = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+    const options = answers.map((answer, index) => (
+        <option key={index} value={index}>
+            {answer}
+        </option>
+    ));
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
+    function handleDeleteQuestion() {
+        fetch("http://localhost:4000/questions/" + id,
+            {
+                method: "DELETE"
+            }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            onDeleteQuestion(id)
+            console.log('Item deleted successfully');
+        })
+            .catch(error => {
+                console.error('There was a problem deleting the item:', error);
+            });
+    }
+
+    function handleUpdateQuestion(e) {
+        const updatedQuestion = {...question, correctIndex: e.target.value}
+        fetch("http://localhost:4000/questions/" + id,
+            {
+                method: "PATCH",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(updatedQuestion)
+            }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            onUpdateQuestion(updatedQuestion)
+        })
+            .catch(error => {
+                console.error('There was a problem updating the item:', error);
+            });
+    }
+
+
+    return (
+        <li>
+            <h4>Question {id}</h4>
+            <h5>Prompt: {prompt}</h5>
+            <label>
+                Correct Answer:
+                <select
+                    defaultValue={correctIndex}
+                    onChange={handleUpdateQuestion}
+                >{options}</select>
+            </label>
+            <button onClick={handleDeleteQuestion}>Delete Question</button>
+        </li>
+    );
 }
 
 export default QuestionItem;
